920f8624b23c5252ca8d6f0524c906fbbc4eb294
Alex McHugh <alexmchughnz@icloud.com>
2024-07-17 22:06:34 +0100

    Update build.

--- build.zig
+++ build.zig
@@ -15,23 +15,16 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "aoc2022-zig",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
+    // Custom: path to specific day.
+    const day_num = b.option(u8, "n", "Day to build");
+    const path_str = if (day_num) |n|
+        std.fmt.allocPrint(b.allocator, "src/day{0d:0>2}/day{0d:0>2}.zig", .{n}) catch unreachable
+    else
+        "src/main.zig";
 
     const exe = b.addExecutable(.{
         .name = "aoc2022-zig",
-        .root_source_file = b.path("src/main.zig"),
+        .root_source_file = b.path(path_str),
         .target = target,
         .optimize = optimize,
     });
@@ -64,16 +57,6 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
@@ -86,6 +69,5 @@ pub fn build(b: *std.Build) void {
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
 }
